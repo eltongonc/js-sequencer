1851b50e667862d7d9a4e7076bf7fa267d84a64b
Elton Gonçalves Gomes <eltongonc@gmail.com>
2017-05-22 04:45:08 +0200

    feature detection, refactored

--- public/js/script.js
+++ public/js/script.js
@@ -1,110 +1,206 @@
+// force remove stuff for test purpose
+// delete window.Audio
+// delete Window.addEventListener
+// delete Document.addEventListener
+// delete Element.addEventListener
+/*******************
+** General
+********************/
+var intervalId; // placeholder for the interval
+
+var feedback = {
+    text: ["Not playing", "Playing"],
+    element: document.querySelector('.feedback p')
+}
+
 /*******************
 ** Feature detection
 ********************/
+// check if the browser supports audio
 if ("Audio" in window) {
     console.log("Audio supported");
+}else {
+    document.body.innerHTML = '<h1 class="feedback">Your browser doesn\'t support html audio</h1><p>This is what your missing(...sorta)<p><iframe width="560" height="315" src="https://www.youtube.com/embed/mIvc6uBDGzU" frameborder="0" allowfullscreen></iframe>'
+}
+
+// add addEventListener to Window if it doesn't exists
+// bad practise according to this post: http://perfectionkills.com/whats-wrong-with-extending-the-dom/
+if (!("addEventListener" in window)) {
+    var f = EventTarget.prototype.addEventListener;
+    EventTarget.prototype.addEventListener = function(type, fn, capture) {
+        this.f = f;
+        this.f(type, fn, capture);
+        alert('Added Event Listener: on' + type);
+    }
 }
 /**************
 ** Instruments
 ***************/
-// list of all the instruments and their names. !!ATTENTION: name = html id
-var allInstruments = [
-    {id: "crash", src:"/sounds/Perc/Crash Future.wav"},
-    {id: "hiHat", src:"/sounds/Hat/Hats14.wav"},
-    {id: "snare", src:"/sounds/Snare/Snare04.wav"},
-    {id: "rightTom", src:"/sounds/Claps/Claps09.wav"},
-    {id: "leftTom", src:"/sounds/Claps/Claps09.wav"},
-    {id: "floorTom", src:"/sounds/Claps/Claps09.wav"},
-    {id: "kick", src:"/sounds/Kick/Kick14.wav"}
-];
-
-// whenever you click on a label make it selected;
-window.addEventListener('click', function(e){
-    if (e.target.localName === "label" ) {
-        e.target.classList.toggle('selected');
+var instruments = {
+    labels: document.querySelectorAll('label'),
+    rows: document.getElementsByClassName('row'),
+    all: // list of all the instruments and their names. !!ATTENTION: name = html id
+    [
+        {id: "crash", src:"/sounds/Perc/Crash Future.wav", icon:"/img/cymbals.svg"},
+        {id: "hiHat", src:"/sounds/Hat/Hats14.wav", icon:"/img/drum-1.svg"},
+        {id: "snare", src:"/sounds/Snare/Snare04.wav", icon:"/img/drum-2.svg"},
+        {id: "rightTom", src:"/sounds/Claps/Claps09.wav", icon:"/img/triangle.svg"},
+        {id: "leftTom", src:"/sounds/Claps/Claps09.wav", icon:"/img/triangle.svg"},
+        {id: "floorTom", src:"/sounds/Claps/Claps09.wav", icon:"/img/triangle.svg"},
+        {id: "kick", src:"/sounds/Kick/Kick14.wav", icon:"/img/drums.svg"}
+    ],
+    init: function(){
+        this.appendIcon();
+        // init app
+        metronome.init();
+    },
+    selected: (function(){
+        // whenever you click on a label make it selected;
+        window.addEventListener('click', function(e){
+            if (e.target.localName === "label" ) {
+                e.target.classList.toggle('selected');
+            }
+        })
+    }()),
+    appendIcon: function(){
+        // set the names of the instruments to the labels
+        for (var i = 0; i < instruments.rows.length; i++) {
+            instruments.rows[i].querySelector('.label').innerHTML = "<img width='50' src='"+instruments.all[i].icon+"'/><span>"+instruments.all[i].id+"</span>";
+        }
     }
-})
-
-/*********************
-** Metronome
-**********************/
-var isPlaying = false;
-var bpm = 150;
-var tempo = 60000 / bpm;
-var intervalId;
-
-// play pause toggle
-var button = document.getElementById('play-pause');
-button.addEventListener('click', playPause);
-function playPause(){
-    if (isPlaying === false) {
-        intervalId = setInterval(metronome, tempo);
-    }else {
-        clearInterval(intervalId);
-    }
-    isPlaying = !isPlaying;
+}
+/*******************
+** BPM and tempo
+********************/
+var bpm = {
+    // Keeps track of the current column.
+    index: 0,
+    value: 150,
+    plusBtn: (function(){
+        // increase tempo
+        createEvent('#plus', "click", updateTempo)
+    }()),
+    minusBtn:(function(){
+        // decrease tempo
+        createEvent('#minus', "click", updateTempo)
+    }()),
+    manual:(function(){
+        // // manual input
+        createEvent('#tempo', "change", updateTempo)
+    }()),
+    update:function(input){bpm.value = input; return bpm.value}, // beat per minute
+    currentTempo: function(){return 60000 / bpm.value;}
 }
 
 function newTempo(){
     clearInterval(intervalId);
-    intervalId = setInterval(metronome, tempo);
+    intervalId = setInterval(metronome.updateRows,bpm.currentTempo());
 }
 
-// increase speed
-var plusBtn = document.getElementById('plus');
-plusBtn.addEventListener('click', updateTempo)
-// decrease speed
-var minusBtn = document.getElementById('minus');
-minusBtn.addEventListener('click', updateTempo)
-// manual input
-var input = document.getElementById('tempo');
-input.addEventListener('change', updateTempo)
-
 function updateTempo(e){
-    if (e.target.id === "minus" && input.value > input.min ) {
-        input.value = Number(input.value)-10;
-    }else if (e.target.id === "plus" && input.value < input.max ) {
-        input.value = Number(input.value)+10;
+    var inputEl = e.target.parentNode.children['tempo'];
+    // slower
+    if (e.target.id === "minus" && inputEl.value > inputEl.min ) {
+       inputEl.value = bpm.update(Number(inputEl.value)-10);
     }
-    if (e.target.id === "tempo") {
-        bpm = e.target.value;
+    // faster
+    else if (e.target.id === "plus" && inputEl.value < inputEl.max ) {
+       inputEl.value = bpm.update(Number(inputEl.value)+10);
     }
-
-    tempo = 60000 / input.value;
-    if(isPlaying === true) {
+    // manual input
+    if (e.target.id === "tempo") {bpm.update(e.target.value);}
+    if(metronome.isPlaying === true) {
         newTempo();
     }
 }
 
-// Sequencer varibles
-var rows = document.getElementsByClassName('row');
-var labels = document.querySelectorAll('label');
-// Beat starts at 1 because 0 is the img for each row
-var beat = 1;
-
-// set the names of the instruments to the labels
-for (var i = 0; i < rows.length; i++) {
-    rows[i].querySelector('.label').innerHTML = allInstruments[i].id;
-}
-
-
-function metronome () {
-    // this is a column of the current beat
-    var currentColumn = document.querySelectorAll('label:nth-of-type('+beat+')');
-    for (var i = 0; i < labels.length; i++) {
-        labels[i].classList.remove('active');
-    }
-    for (var i = 0; i < currentColumn.length; i++) {
-        currentColumn[i].classList.add('active');
-        // for every checkbox that is checked play the corresponding Audio
-        if (currentColumn[i].children[0].checked) {
-            new Audio(allInstruments[i].src).play();
+/*******************
+** metronome
+********************/
+var metronome = {
+    isPlaying: false, //current state of the metronome
+    init:function(){
+        // play pause toggle
+        createEvent('#play-pause', "click", metronome.startStop);
+    },
+    getColumn:function(){
+        if ( bpm.index < (instruments.rows[0].children.length - 1) ) {
+            ++bpm.index;
+        }
+        // set it to 1 because position 0 is the instrument label
+        else { bpm.index = 1;}
+        return document.querySelectorAll('label:nth-of-type('+bpm.index+')');
+    },
+    startStop: function(){
+        if (metronome.isPlaying === false) {
+            feedback.element.innerHTML = feedback.text[1]
+            intervalId = setInterval(metronome.updateRows,bpm.currentTempo());
+        }else {
+            feedback.element.innerHTML = feedback.text[0]
+            clearInterval(intervalId);
+        }
+        metronome.isPlaying = !metronome.isPlaying;
+    },
+    updateRows: function(){
+        var currentColumn = metronome.getColumn();
+        for (var i = 0; i < instruments.labels.length; i++) {
+            instruments.labels[i].classList.remove('active');
+        }
+        for (var i = 0; i < currentColumn.length; i++) {
+            currentColumn[i].classList.add('active');
+            // for every checkbox that is checked play the corresponding Audio
+            if (currentColumn[i].children[0].checked) {
+                new Audio(instruments.all[i].src).play();
+            }
         }
     }
-    // If we get to the last child, start over
-    if ( beat < (rows[0].children.length - 1) ) {
-        ++beat;
-    } else {
-        beat = 1;
-    }
 }
+
+
+/*************************
+** keyboard input
+**************************/
+var controls = {
+    keyboard: (function(){
+        window.addEventListener("keydown", function(event){
+            event.preventDefault()
+            var id = "key-"+event.key;
+            if (event.code !== "Space" && document.getElementById(id)) {
+                var element = document.getElementById(id);
+                var parent = element.parentNode;
+                var grandParent = parent.parentNode;
+                element.checked = !element.checked;
+                element.checked?
+                    parent.classList.add('selected') :
+                    parent.classList.remove('selected')
+                instruments.all.filter(function(instr){
+                    if (instr.id === grandParent.id) {
+                        var newAudio = new Audio(instr.src);
+                        newAudio.play();
+                    }
+                })
+            }
+            if (event.code == "Space" || event.code == "Enter") {
+                playPause()
+            }
+        });
+        window.addEventListener("keyup", function(event){
+            var id = "key-"+event.key;
+            if (event.code !== "Space" && document.getElementById(id)) {
+                var src = document.getElementById(id);
+            }
+        });
+    }())
+};
+
+/*********************
+** helpers
+**********************/
+function createEvent(selector, eventType, eventFunction){
+    var element = document.querySelector(selector);
+    element.addEventListener(eventType,eventFunction);
+    return element;
+}
+
+//build instrument
+instruments.init();
